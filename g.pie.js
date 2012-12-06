2bbd8dbc7f5a82cb5d5dadca64e64318513123c2
jboland <josephjboland@gmail.com>
2012-12-06 10:09:38 -0500

    Update g.pie.js

--- g.pie.js
+++ g.pie.js
@@ -95,10 +95,12 @@
                 values[i] = { value: values[i], order: i, valueOf: function () { return this.value; } };
             }
             
-            //values are sorted numerically
-            values.sort(function (a, b) {
-                return b.value - a.value;
-            });
+            //values are sorted numerically IFF opts not provided
+            if(!opts.sort){
+             values.sort(function (a, b) {
+                 return b.value - a.value;
+             });
+            }
             
             for (i = 0; i < len; i++) {
                 if (defcut && values[i] * 100 / total < minPercent) {
